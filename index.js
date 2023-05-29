f795a8fba317c310524ef806f441250111373222
hugo <hugo-mehdi.taj@epitech.eu>
2023-05-29 23:01:45 +0200

    [add] Js Finish

--- index.js
+++ index.js
@@ -1,89 +1,65 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
-const path = require("path");
+const path = require('path');
 
 const app = express();
 const port = 8080;
+const listePokemon = ["bulbizarre","herbizarre","florizarre","salameche","reptincel","dracaufeu","carapuce","carabaffe","tortank","chenipan","chrysacier","papilusion","aspicot","coconfort","dardargnan","roucool","roucoups","roucarnage","rattata","rattatac","piafabec","rapasdepic","abo","arbok","pikachu","raichu","sabelette","sablaireau","nidoran","nidorina","nidoqueen","nirodan","nidorino","nidoking","melofee","melodelfe","goupix","feunard","rondoudou","grodoudou","nosferapti","nosferalto","mystherbe","ortide","rafflesia","paras","parasect","mimitoss","aeromite","taupiqueur","triopikeur","miaouss","persian","psykokwak","akwakwak","ferosinge","colossinge","caninos","arcanin","ptitard","tetarte","tartard","abra","kadabra","alakazam","machoc","machopeur","mackogneur","chetiflor","boustiflor","empiflor","tentacool","tentacruel","racaillou","gravalanch","grolem","ponyta","galopa","ramoloss","flagadoss","magneti","magneton","canarticho","doduo","dodrio","otaria","lamantine","tadmorv","grotadmorv","kokiyas","crustabri","fantominus","spectrum","ectoplasma","onix","soporifik","hypnomade","krabby","krabboss","voltorbe","electrode","noeunoeuf","noadkoko","osselait","ossatueur","kicklee","tygnon","excelangue","smogo","smogogo","rhinocorne","rhinoferos","leveinard","saquedeneu","kangourex","hypotrempe","hypocean","poissirene","poissoroy","stari","staross","mime","insecateur","lippoutou","elektek","magmar","scarabrute","tauros","magicarpe","leviator","lokhlass","metamorph","evoli","aquali","voltali","pyroli","porygon","amonita","amonistar","kabuto","kabutops","ptera","ronflex","artikodin","electhor","sulfura","minidraco","draco","dracolosse"]
 
-app.use(express.static(path.join(__dirname,"/templates")))
+app.use(express.static(path.join(__dirname, '/templates')));
 
-app.get('/api/', async (req, res) => {
-  /*
-  const searchTerm = req.query.search;
-  
-  if (!searchTerm) {
-    res.send('Please provide a search term.');
-    return;
-  }
-  */
+// Route pour récupérer les informations spécifiques d'un Pokémon en fonction de son ID
+app.get('/api/:pokemonId', async (req, res) => {
+  const pokemonId = req.params.pokemonId;
+  let i = 0;
+  for(i; i < listePokemon.length - 1; i++) {
+    if(pokemonId.toLowerCase() === listePokemon[i].toLowerCase()) {
+      break;
+    }
+  };
 
   try {
-    const browser = await puppeteer.launch(
-      {headless: false}
-    );
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto(`https://eternia.fr/fr/pokedex/liste-pokemon/`);
-    await page.waitForSelector('table');
-    
-    //const links = await page.$$eval('td a', (anchors) => {
-    //  return anchors.map((anchor) => anchor.href);
-    //});
-    //console.log(links)
-    const pokemonData = [];
+    // Accéder à la page du Pokémon en utilisant l'ID fourni
+    await page.goto(`https://eternia.fr/fr/pokedex/pokemon/${i+1}`);
 
-    let links = []
-    for (let i = 1; i < 150; i++) {
-      links.push("https://eternia.fr/fr/pokedex/pokemon/" + i)
-    }
+    // Extraction des informations du Pokémon de la page
+    const pokemonData = await page.evaluate(() => {
+      const name = document.querySelector('#content > h1').innerText;
+      const number = document.querySelector('#content > div.bloc_infos_generales > div.column_infos_g > table > tbody > tr:nth-child(1) > td').innerText.trim();
+      const types = document.querySelector('#content > div.bloc_infos_generales > div.column_infos_g > table > tbody > tr:nth-child(2) > td').innerText.trim().split('\n');
+      const height = document.querySelector('#content > div.bloc_infos_generales > div.column_infos_g > table > tbody > tr:nth-child(4) > td').innerText.trim();
+      const weight = document.querySelector('#content > div.bloc_infos_generales > div.column_infos_g > table > tbody > tr:nth-child(5) > td').innerText.trim();
+      const hp = parseInt(document.querySelector('#content > div.bloc_infos_generales > div.column_infos_d > table.tab_statsB > tbody > tr:nth-child(1) > td.statNb').innerText.trim(), 10);
+      const attack = parseInt(document.querySelector('#content > div.bloc_infos_generales > div.column_infos_d > table.tab_statsB > tbody > tr:nth-child(2) > td.statNb').innerText.trim(), 10);
+      const defense = parseInt(document.querySelector('#content > div.bloc_infos_generales > div.column_infos_d > table.tab_statsB > tbody > tr:nth-child(3) > td.statNb').innerText.trim(), 10);
+      const spAttack = parseInt(document.querySelector('#content > div.bloc_infos_generales > div.column_infos_d > table.tab_statsB > tbody > tr:nth-child(4) > td.statNb').innerText.trim(), 10);
+      const spDefense = parseInt(document.querySelector('#content > div.bloc_infos_generales > div.column_infos_d > table.tab_statsB > tbody > tr:nth-child(5) > td.statNb').innerText.trim(), 10);
+      const speed = parseInt(document.querySelector('#content > div.bloc_infos_generales > div.column_infos_d > table.tab_statsB > tbody > tr:nth-child(6) > td.statNb').innerText.trim(), 10);
+      const image = document.querySelector('#content > div.bloc_infos_generales > div.ImgSuivPrec > div.artwork_off > img').src
 
-    for (const link of links) {
-      await page.goto(link);
-
-
-      //await page.waitForSelector('.content_fiche');
-      await page.waitForSelector('.pokedex');
-
-      const data = await page.evaluate(() => {
-        //const number = document.getElementsByClassName('.fiche_titre span').innerText;
-        const tag = document.getElementsByClassName('pokedex')[0].innerText;
-        const description = document.getElementsByClassName('tab_infoG')[0].innerText;
-        const stat = document.getElementsByClassName('tab_statsB')[0].innerText;
-        //const height = document.querySelector('.fiche_texte .taille strong').innerText;
-        //const weight = document.querySelector('.fiche_texte .poids strong').innerText;
-        //const hp = document.querySelector('.fiche_texte .statistiques .pv strong').innerText;
-        //const attack = document.querySelector('.fiche_texte .statistiques .attaque strong').innerText;
-        //const defense = document.querySelector('.fiche_texte .statistiques .defense strong').innerText;
-        //const spAttack = document.querySelector('.fiche_texte .statistiques .attaque_spe strong').innerText;
-        //const spDefense = document.querySelector('.fiche_texte .statistiques .defense_spe strong').innerText;
-        //const speed = document.querySelector('.fiche_texte .statistiques .vitesse strong').innerText;
-
-        console.log(tag);
-        console.log(description);
-        console.log(stat);
-        
-        return {
-          //number,
-          tag,
-          description,
-          stat,
-          //height,
-          //weight,
-          //hp,
-          //attack,
-          //defense,
-          //spAttack,
-          //spDefense,
-          //speed
-        };
-      });
-
-
-      pokemonData.push(data);
-    }
+      // Retourner les informations du Pokémon
+      return {
+        name,
+        number,
+        types,
+        height,
+        weight,
+        hp,
+        attack,
+        defense,
+        spAttack,
+        spDefense,
+        speed,
+        image,
+      };
+    });
 
     await browser.close();
 
+    // Renvoyer les données du Pokémon au format JSON
     res.json(pokemonData);
   } catch (error) {
     console.error('An error occurred:', error);
@@ -91,18 +67,17 @@ app.get('/api/', async (req, res) => {
   }
 });
 
+// Route pour la page d'accueil
 app.get('/', async (req, res) => {
-
-    res.sendFile(__dirname+"/templates/html/index.html");
-
+  res.sendFile(path.join(__dirname, '/templates/html/index.html'));
 });
 
+// Route pour la page de chaque Pokémon
 app.get('/:pokemon', async (req, res) => {
-
-    res.sendFile(__dirname+"/templates/html/pokemon.html");
-
+  res.sendFile(path.join(__dirname, '/templates/html/pokemon.html'));
 });
 
+// Démarrer le serveur
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}/`);
 });
--- templates/script/script.js
+++ templates/script/script.js
@@ -2,8 +2,6 @@ fetch('http://localhost:8080/api')
     .then(data => data.json())
     .then(data => {
         data = data[0]
-        console.log(data)
         const title = document.querySelector("h2#pokemon-name");
-        title.innerHTML = data.tag.split(" ")[3].split("\n")[0];
-
+        title.innerHTML = data.tag;
 })
